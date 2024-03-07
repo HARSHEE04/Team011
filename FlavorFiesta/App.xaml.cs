579a3fb752e9000091842e4d5cd1e6b0eeba8e90
MaryamElhamidi <unicolmaryame@gmail.com>
2024-03-07 12:10:35 -0500

    Imported Maui App

---
+++ FlavorFiesta/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace FlavorFiesta;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
+
---
+++ FlavorFiesta/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace FlavorFiesta;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
