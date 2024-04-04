3ea6838c0184559c14c1f999f8a635b63b432d9e
Harsheta <harshetasharma5@gmail.com>
2024-04-04 17:12:48 -0400

    recipe page created

---
+++ FlavorFiesta/BusinessLogic/Recipe.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlavorFiesta.BusinessLogic
+{
+    internal class Recipe
+    {
+        private int id;
+    }
+}
