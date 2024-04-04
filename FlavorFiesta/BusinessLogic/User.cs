0288dfc0c35912c22c64409fd8f98ecb4d861af8
SarasGunjan <saraswag@sheridancollege.ca>
2024-04-04 17:44:27 -0400

    User classes Properties

--- FlavorFiesta/BusinessLogic/User.cs
+++ FlavorFiesta/BusinessLogic/User.cs
@@ -13,5 +13,53 @@ namespace FlavorFiesta.BusinessLogic
         string _password;
         DateTime _dateOfBirth;
         Random _random;
+
+        public string Name
+        {
+            get => Name;
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                    throw new ArgumentNullException("Name cannot be blank.");
+                Name = value;
+            }
+        }
+
+        public string Email
+        {
+            get => Email;
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentNullException("Email cannot be blank.");
+                }
+                Email = value;
+            }
+        }
+
+        public string Password
+        {
+            get => Password;
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                    throw new Exception("Password cannot be blank.");
+                else if (value.Length < 6)
+                    throw new Exception("Password must contain more than 6 characters.");
+                Password = value;
+            }
+        }
+
+        public DateTime DateOfBirth
+        {
+            get => DateOfBirth;
+            set
+            {
+                if (value == DateTime.Today)
+                    throw new Exception("Date of birth must be greater than today's date.");
+                DateOfBirth = value;
+            }
+        }
     }
 }
