f27fe9334932ed36314da9bc59cd3c71ed857094
MaryamElhamidi <unicolmaryame@gmail.com>
2024-04-04 16:42:26 -0400

    Rough Preferences Page Complete

---
+++ FlavorFiesta/Pages/NutritionalPreferencesPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows.Input;
+
+namespace FlavorFiesta.Pages;
+
+public partial class NutritionalPreferencesPage : ContentPage
+{
+    public NutritionalPreferencesPage()
+    {
+        InitializeComponent();
+        BindingContext = this;
+    }
+
+    public ICommand SubmitCommand => new Command(OnSubmit);
+
+    private void OnSubmit()
+    {
+        // still have to handle the submit action
+    }
+}
