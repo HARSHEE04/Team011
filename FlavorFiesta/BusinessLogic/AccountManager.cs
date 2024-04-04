6439e229d1d0031b79753915593bee44109c0979
SarasGunjan <saraswag@sheridancollege.ca>
2024-04-04 19:46:48 -0400

    AccountManager RemoveUser created

--- FlavorFiesta/BusinessLogic/AccountManager.cs
+++ FlavorFiesta/BusinessLogic/AccountManager.cs
@@ -8,5 +8,32 @@ namespace FlavorFiesta.BusinessLogic
 {
     public class AccountManager
     {
+        Dictionary<string, User> _users = new Dictionary<string, User>();
+
+        public Dictionary<string, User> Users => _users;
+
+        public Dictionary<string, User> AddUser(User user)
+        {
+            foreach (string key in _users.Keys)
+            {
+                if (key == user.Email)
+                    throw new Exception("This user already exists");
+
+            }
+
+            Dictionary<string, User> dict = new Dictionary<string, User>();
+            dict.Add(user.Email, user);
+            return dict;
+        }
+
+        public void RemoveUser(User user)
+        {
+            foreach (string email in _users.Keys)
+            {
+                if (email == user.Email)
+                    Users.Remove(user.Email);
+            }
+        }
+
     }
 }
