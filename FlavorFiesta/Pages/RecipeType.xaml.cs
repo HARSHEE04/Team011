61d50c62e8d2772d6170d973f85fda951302707d
MaryamElhamidi <unicolmaryame@gmail.com>
2024-04-09 08:38:12 -0400

    RecipeType XAML completed

---
+++ FlavorFiesta/Pages/RecipeType.xaml.cs
@@ -0,0 +1,25 @@
+﻿namespace FlavorFiesta.Pages;
+
+public partial class RecipeType : ContentPage
+{
+	public RecipeType()
+	{
+		InitializeComponent();
+	}
+
+
+    private void OnMealTimeChanged(object sender, CheckedChangedEventArgs e)
+    {
+        var radioButton = sender as RadioButton;
+        if (e.Value)
+        {
+            // TODO: handle the logic when a meal time is selected.
+            string selectedMealTime = radioButton.Content.ToString();
+        }
+    }
+
+    private async void OnSubmitClicked(object sender, EventArgs e)
+    {
+        // TODO: implement what happens when the submit button is clicked
+    }
+}
