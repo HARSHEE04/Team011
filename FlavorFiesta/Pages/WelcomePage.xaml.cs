50342bb1f3bd21299b6ceb8050b98f7eff34cadb
MaryamElhamidi <unicolmaryame@gmail.com>
2024-04-09 04:11:49 -0400

    Made the signup button navigate you to PreferencePage

--- FlavorFiesta/Pages/WelcomePage.xaml.cs
+++ FlavorFiesta/Pages/WelcomePage.xaml.cs
@@ -2,8 +2,21 @@
 
 public partial class WelcomePage : ContentPage
 {
-	public WelcomePage()
-	{
-		InitializeComponent();
-	}
+    public WelcomePage()
+    {
+        InitializeComponent();
+        BindingContext = this;
+    }
+
+    private async void OnSignInClicked(object sender, EventArgs e)
+    {
+        // Will Work On Later
+    }
+
+    private async void OnSignUpClicked(object sender, EventArgs e)
+    {
+        // Navigate to the Sign-Up Page
+        await Navigation.PushAsync(new NutritionalPreferencesPage());
+
+    }
 }
