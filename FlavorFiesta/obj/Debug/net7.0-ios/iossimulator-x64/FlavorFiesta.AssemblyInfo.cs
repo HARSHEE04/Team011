2251c713ee43c8553709c5dccfa0e51ef36d3251
Harsheta <harshetasharma5@gmail.com>
2024-04-08 22:57:43 -0400

    Recipe manager class created

--- FlavorFiesta/obj/Debug/net7.0-ios/iossimulator-x64/FlavorFiesta.AssemblyInfo.cs
+++ FlavorFiesta/obj/Debug/net7.0-ios/iossimulator-x64/FlavorFiesta.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FlavorFiesta")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+0b1cbb38faeebb0239326d7c2d7647c3df336768")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+2e9bfbbd0f804332b8c01e684a7add2e91bbad1c")]
 [assembly: System.Reflection.AssemblyProductAttribute("FlavorFiesta")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FlavorFiesta")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
