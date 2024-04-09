19995fa37f9c5d699d232476a5dea6a54822f09b
MaryamElhamidi <unicolmaryame@gmail.com>
2024-04-09 08:45:13 -0400

    CusinePage Xaml Completed

---
+++ FlavorFiesta/Pages/CusinePage.xaml.cs
@@ -0,0 +1,23 @@
+﻿namespace FlavorFiesta.Pages;
+
+public partial class CusinePage : ContentPage
+{
+	public CusinePage()
+	{
+		InitializeComponent();
+	}
+
+    private void OnCuisineTypeChanged(object sender, CheckedChangedEventArgs e)
+    {
+        var radioButton = sender as RadioButton;
+        if (e.Value)
+        {
+            // TODO: Handle the logic when a cuisine type is selected.
+            string selectedCuisineType = radioButton.Content.ToString();
+        }
+    }
+
+    private async void OnSubmitClicked(object sender, EventArgs e)
+    {
+    }
+}
