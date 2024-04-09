6bcbcec547774264844fe565a0a94cf7f61c6d94
MaryamElhamidi <unicolmaryame@gmail.com>
2024-04-09 08:24:02 -0400

    FoodChoiceOptions Xaml Completed

---
+++ FlavorFiesta/Pages/FoodChoiceOptions.xaml.cs
@@ -0,0 +1,28 @@
+﻿namespace FlavorFiesta.Pages;
+
+public partial class FoodChoiceOptions : ContentPage
+{
+    public FoodChoiceOptions()
+    {
+        InitializeComponent();
+    }
+
+    private void OnFoodChoiceChanged(object sender, CheckedChangedEventArgs e)
+    {
+        var radioButton = sender as RadioButton;
+        if (e.Value)
+        {
+            string selectedFoodChoice = radioButton.Content.ToString();
+            DisplayAlert("Food Choice Selected", $"You have selected: {selectedFoodChoice}", "OK");
+
+            //for the future: 
+            // set a property to the selected value.
+            // ex: viewModel.SelectedFoodChoice = selectedFoodChoice;
+
+            // navigate to another page based on the selection, you could do that here.
+
+            // saving the choice to persistent storage
+            // ex: Preferences.Set("foodChoice", selectedFoodChoice);
+        }
+    }
+}
