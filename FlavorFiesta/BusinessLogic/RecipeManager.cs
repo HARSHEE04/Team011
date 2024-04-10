1ef4f9b0badd92e196bc6e778193887ecbde29fe
Harsheta <harshetasharma5@gmail.com>
2024-04-10 17:33:07 -0400

    method made to read the recipes from the csv and create recipe objects with the preferences object which is in association with recipe class

--- FlavorFiesta/BusinessLogic/RecipeManager.cs
+++ FlavorFiesta/BusinessLogic/RecipeManager.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -24,7 +25,7 @@ namespace FlavorFiesta.BusinessLogic
             {
                 string[] recipeObjects = File.ReadAllLines(_filePath);//used file path because that is the location, each line will have a recipe object
 
-                foreach(string line in recipeObjects)
+                foreach (string line in recipeObjects)
                 {
                     string[] parts = line.Split(',');
 
@@ -32,34 +33,56 @@ namespace FlavorFiesta.BusinessLogic
                     string name = parts[0];
                     string recipeImage = parts[1];
                     string recipeURL = parts[2];
-                    string allRecipePreferences = parts[3];
 
 
                     //now create the preferences object for this specific recipe 
-                    Preferences recipePrefrences= new Preferences
+                    Preferences preferences = new Preferences(
+
+                        //the info and its corresponding property which will be used in Recipe class object made below
+                        parts[3], // dietType
+                        parts[4], // cuisineType
+                        parts[5], // mealType
+                        int.Parse(parts[6]), // caloriesRange
+                        int.Parse(parts[7]), // proteinRange
+                        int.Parse(parts[8]), // sugarRange
+                        int.Parse(parts[9]), // servingsRange
+                        TimeSpan.Parse(parts[10]), // prepTimeRange
+                        new List<string>(parts[11].Split(';')) // dietaryRestrictions
+                    );
+
+                    //new recipe object made
+                    Recipe recipe = new Recipe(name, recipeImage, recipeURL, preferences);
+                    //add this recipe to the list of recipes
+                    list.Add(recipe);
                 }
-               
-            } 
 
+                }
+                  catch (Exception ex)
+            {
+                Console.WriteLine("Error reading recipes: " + ex.Message);
+            }
+
+            return list;
         }
-        ///read recipes from csv file and make it into a list
-
-   
-        ///convert the things read from csv to individual preference properties get DIETTYPE= parts[1]
-        ///make a method to get preferences object with specific selected preferences
-        ///make a recipe search metod that compares the preference property to the recipe properties in the csv
-        ///return two recipes that match this.
-        //nutrition class has the info about the meal type, diet type, cusine type, calories, protein, sugar, sering, prep time and dietary restirctions
-        //The recipe class needs to have a composition relationship with the nutrition class to get the values of these 
-        //then recipe manager class needs to check if the recipes property== preferencees.property, if it does, then return those recipes.
-
-        //TRY FROM WHERE SELECT SYNTAX https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/keywords/select-clause
-
-        //figure out way to only get two recipes from all the matches, try TAKE() METHOD: https://www.tutorialspoint.com/chash-queryable-take-method
-
-        //can use list to hold all recipe objects,can idenitfy the recipes using ID property
-
-        //ASK IF WE NEED TO USE ABSTARCT CLASS AND INTERFACES?
-        //dfsdsdfs
     }
 }
+
+
+///convert the things read from csv to individual preference properties get DIETTYPE= parts[1]
+///make a method to get preferences object with specific selected preferences
+///make a recipe search metod that compares the preference property to the recipe properties in the csv
+///return two recipes that match this.
+//nutrition class has the info about the meal type, diet type, cusine type, calories, protein, sugar, sering, prep time and dietary restirctions
+//The recipe class needs to have a composition relationship with the nutrition class to get the values of these 
+//then recipe manager class needs to check if the recipes property== preferencees.property, if it does, then return those recipes.
+
+//TRY FROM WHERE SELECT SYNTAX https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/keywords/select-clause
+
+//figure out way to only get two recipes from all the matches, try TAKE() METHOD: https://www.tutorialspoint.com/chash-queryable-take-method
+
+//can use list to hold all recipe objects,can idenitfy the recipes using ID property
+
+//ASK IF WE NEED TO USE ABSTARCT CLASS AND INTERFACES?
+//dfsdsdfs
+
+
