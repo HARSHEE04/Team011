e10b62284b9b8d71e41aca1ab020bc172f92d3d2
MaryamElhamidi <unicolmaryame@gmail.com>
2024-04-11 19:20:51 -0400

    Added Page & Edited PrefManager

--- FlavorFiesta/BusinessLogic/PreferencesManager.cs
+++ FlavorFiesta/BusinessLogic/PreferencesManager.cs
@@ -1,30 +1,47 @@
 ﻿using System;
+using System.Text.Json;
+
 namespace FlavorFiesta.BusinessLogic
 {
 	public class PreferencesManager
 	{
-		//define a list
+        //define a list
+        private List<Preferences> _preferencesList = new List<Preferences>();
+        private readonly string _filePath;
+
+        public PreferencesManager(string filePath)
+        {
+            _filePath = filePath;
+            LoadPreferences();
+        }
+        public void AddPreferences(Preferences preferences)
+        {
+            _preferencesList.Add(preferences);
+            SavePreferences();
+        }
+        public IEnumerable<Preferences> GetUserPreferences() => _preferencesList;
+
+        private void SavePreferences()
+        {
+            var options = new JsonSerializerOptions { WriteIndented = true };
+            File.WriteAllText(_filePath, JsonSerializer.Serialize(_preferencesList, options));
+        }
 
 
-		// keeps a track of all previous preferences, each time user chooses
-		// preferences, we add it to the list of preferences so user has access 
-        public PreferencesManager()
-		{
-		
-		}
-		private void AddPrefernces()
-		{
-			//return it to the prefernces class as a list
-			// add to the list
-			return; 
-		}
-
-		private void RetriveUserInfo()
-		{
-			//takes the user information, and takes them into a listview page where
-			//they can either do previous prefernces or new prefernces
-			return;
-		}
+        private void LoadPreferences()
+        {
+            if (File.Exists(_filePath))
+            {
+                string jsonString = File.ReadAllText(_filePath);
+                _preferencesList = JsonSerializer.Deserialize<List<Preferences>>(jsonString) ?? new List<Preferences>();
+            }
+        }
+  //      private void RetriveUserInfo()
+		//{
+		//	//takes the user information, and takes them into a listview page where
+		//	//they can either do previous prefernces or new prefernces
+		//	return;
+		//}
 	}
 }
 
---
+++ FlavorFiesta/Pages/PreviousPreferences.xaml.cs
@@ -0,0 +1,23 @@
+﻿using FlavorFiesta.BusinessLogic;
+using Microsoft.Maui.Controls;
+using System;
+
+namespace FlavorFiesta.Pages;
+
+public partial class NutritionalPreferencesSelectionPage : ContentPage
+{
+    private PreferencesManager _preferencesManager;
+
+    public NutritionalPreferencesSelectionPage(PreferencesManager preferencesManager)
+    {
+        InitializeComponent();
+        _preferencesManager = preferencesManager;
+        PreferencesListView.ItemsSource = _preferencesManager.GetUserPreferences();
+    }
+
+    private void OnCreateNewPreferencesClicked(object sender, EventArgs e)
+    {
+        // Navigate to the NutritionalPreferencesPage for creating new preferences
+        Navigation.PushAsync(new NutritionalPreferencesPage());
+    }
+}
