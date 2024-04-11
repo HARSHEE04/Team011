b7253490554ffc9c1078a26ca4a9ab82c3126702
SarasGunjan <saraswag@sheridancollege.ca>
2024-04-11 14:05:01 -0400

    Sign up page password settings changed

--- FlavorFiesta/Pages/SignUpPage.xaml.cs
+++ FlavorFiesta/Pages/SignUpPage.xaml.cs
@@ -22,27 +22,19 @@ public partial class SignUpPage : ContentPage
         string email = EmailEntry.Text;
         DateTime dateOfBirth = DatePicker.Date;
         string password = PasswordEntry.Text;
-        string confirmPassword = ConfirmPasswordEntry.Text;
-        //if (methodConfirmPassword(password, confirmPassword) == false)
-        //{
-        //    DisplayAlert("Incorrect Password", "This password doesn't match with the original password", "Ok");
-         
-
-        //}
-
+            
         string fullName = firstName + " " + lastName;
-        User user1 = new User(fullName, email, password, dateOfBirth);
-        return user1; 
+     
+        return new User(fullName, email, password, dateOfBirth); 
 
 
     }
     void OnSignUpButton(object sender, EventArgs e)
     {
         // Implementation for what should happen when the button is clicked
+        _manager.AddUser(CaptureUserInput());
     }
 
-    void OnViewUsers(System.Object sender, System.EventArgs e)
-    {
-    }
+    
 
 }
