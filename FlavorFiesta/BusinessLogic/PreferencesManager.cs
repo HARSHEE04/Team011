6269e10d89ba9a5a19e7cf5de1c8166a7c2ddcb5
MaryamElhamidi <unicolmaryame@gmail.com>
2024-04-11 12:41:40 -0400

    Added PreferencesManger

--- FlavorFiesta/BusinessLogic/PreferencesManager.cs
+++ FlavorFiesta/BusinessLogic/PreferencesManager.cs
@@ -3,7 +3,9 @@ namespace FlavorFiesta.BusinessLogic
 {
 	public class PreferencesManager
 	{
-		public PreferencesManager()
+        // keeps a track of all previous preferences, each time user chooses
+		// preferences, we add it to the list of preferences so user has access 
+        public PreferencesManager()
 		{
 		}
 	}
