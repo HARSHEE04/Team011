9e713c8066880445a49e1d2a8ebda1c26c122beb
SarasGunjan <saraswag@sheridancollege.ca>
2024-04-12 00:44:32 -0400

    Account Manager Updated

--- FlavorFiesta/BusinessLogic/AccountManager.cs
+++ FlavorFiesta/BusinessLogic/AccountManager.cs
@@ -11,34 +11,11 @@ namespace FlavorFiesta.BusinessLogic
         Dictionary<string, User> _users = new Dictionary<string, User>();
 
         public Dictionary<string, User> Users => _users;
-
-        //public void AddUser(User user)
-        //{
-        //    foreach (string key in _users.Keys)
-        //    {
-        //        if (key == user.Email)
-        //            throw new Exception("This user already exists");
-
-        //    }
-
-        //    Dictionary<string, User> dict = new Dictionary<string, User>();
-        //    dict.Add(user.Email, user);
-
-        //}
         public void AddUser(User user)
         {
             if (_users.ContainsKey(user.Email))
                 throw new Exception("This user already exists");
             _users[user.Email] = user;
-        }
-
-        public void RemoveUser(User user)
-        {
-            foreach (string email in _users.Keys)
-            {
-                if (email == user.Email)
-                    Users.Remove(user.Email);
-            }
         }
 
         public bool SearchUser(string emailEntry)
