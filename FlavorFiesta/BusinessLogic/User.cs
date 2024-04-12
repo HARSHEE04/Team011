009e586e18e8ba82b76546b4c143d28a1f3536e6
MaryamElhamidi <unicolmaryame@gmail.com>
2024-04-12 03:02:53 -0400

    Update User.cs

--- FlavorFiesta/BusinessLogic/User.cs
+++ FlavorFiesta/BusinessLogic/User.cs
@@ -20,7 +20,7 @@ namespace FlavorFiesta.BusinessLogic
             {
                 if (string.IsNullOrEmpty(value))
                     throw new ArgumentNullException("Name cannot be blank.");
-                Name = value;
+                _name = value;
             }
         }
 
@@ -33,7 +33,7 @@ namespace FlavorFiesta.BusinessLogic
                 {
                     throw new ArgumentNullException("Email cannot be blank.");
                 }
-                Email = value;
+                _email = value;
             }
         }
 
@@ -46,7 +46,7 @@ namespace FlavorFiesta.BusinessLogic
                     throw new Exception("Password cannot be blank.");
                 else if (value.Length < 6)
                     throw new Exception("Password must contain more than 6 characters.");
-                Password = value;
+                _password = value;
             }
         }
 
@@ -57,7 +57,7 @@ namespace FlavorFiesta.BusinessLogic
             {
                 if (value == DateTime.Today)
                     throw new Exception("Date of birth must be greater than today's date.");
-                DateOfBirth = value;
+                _dateOfBirth = value;
             }
         }
 
