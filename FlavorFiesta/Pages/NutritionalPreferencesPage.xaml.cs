f62882a1c419304afa59783f3728bc226a3c1b85
HARSHEE04 <harshetasharma5@gmail.com>
2024-04-12 13:42:04 -0400

    Update NutritionalPreferencesPage.xaml.cs
    helped maryam debug her preferences code behind, we decided to implement the same type of logic as before and create radio button event changed methods in the code behind

--- FlavorFiesta/Pages/NutritionalPreferencesPage.xaml.cs
+++ FlavorFiesta/Pages/NutritionalPreferencesPage.xaml.cs
@@ -7,46 +7,120 @@ namespace FlavorFiesta.Pages
 {
     public partial class NutritionalPreferencesPage : ContentPage
     {
-        private BusinessLogic.Preferences _prefs;
+        private string _dietType;
+        private string _cuisineType;
+        private string _recipeType;
+        private string _caloriesRange;
+        private string _proteinRange;
+        private string _sugarRange;
+        private string _servingsRange;
+        private TimeSpan _prepTimeRange;
+        private List<string> _dietaryRestrictions;
 
-        public NutritionalPreferencesPage(BusinessLogic.Preferences prefs)
+
+        public NutritionalPreferencesPage(string dietType, string cuisineType, string recipeType)
         {
             InitializeComponent();
-            BindingContext = this;
-            _prefs = prefs;
+            //BindingContext = this;
+            _dietType = dietType;
+            _cuisineType = cuisineType;
+            _recipeType = recipeType;
+
+
         }
 
+        //make an object of preferences and send it to the on submit
         // Event handler for when the Submit button is clicked
         private void OnSubmit(object sender, EventArgs e)
         {
-            Navigation.PushAsync(new ChooseRecipe(_prefs));
+            Navigation.PushAsync(new ChooseRecipe());
         }
+
+
+
         // Sugar
-        private void OnSugarSelectionChanged(object sender, CheckedChangedEventArgs e)
+        private async void OnSugarRangeChanged(object sender, CheckedChangedEventArgs e)
         {
-            if (e.Value)
+            var radioButton = sender as RadioButton;
+            if (e.Value == true)
             {
-                _prefs.SugarRange = GetSugarRangeFromRadioButton(sender as RadioButton);
+                // Set the selected cuisine type to preferences
+                string selectedSugarRange = radioButton.Content.ToString();
+
+                //_prefs.CuisineType = selectedCuisineType;
+                _sugarRange = selectedSugarRange;
+                
             }
+
+
         }
 
-        // Servings
-        private void OnServingsSelectionChanged(object sender, CheckedChangedEventArgs e)
+        // Calories
+        private async void OnCaloriesRangeChanged(object sender, CheckedChangedEventArgs e)
         {
-            if (e.Value)
+            var radioButton = sender as RadioButton;
+            if (e.Value == true)
             {
-                _prefs.ServingsRange = GetServingsRangeFromRadioButton(sender as RadioButton);
+                // Set the selected cuisine type to preferences
+                string selectedCaloriesRange = radioButton.Content.ToString();
+
+                //_prefs.CuisineType = selectedCuisineType;
+                _caloriesRange = selectedCaloriesRange; 
+               
             }
+
+
+        }
+        // Protein
+        private async void OnProtienRangeChanged(object sender, CheckedChangedEventArgs e)
+        {
+            var radioButton = sender as RadioButton;
+            if (e.Value == true)
+            {
+                // Set the selected cuisine type to preferences
+                string selectedProtienType = radioButton.Content.ToString();
+
+                //_prefs.CuisineType = selectedCuisineType;
+                _proteinRange = selectedProtienType;
+                
+            }
+
+
         }
 
-        // Prep Time
-        private void OnPrepTimeSelectionChanged(object sender, CheckedChangedEventArgs e)
+        // Number of servings
+        private async void OnNumberofServingsChanged(object sender, CheckedChangedEventArgs e)
         {
-            if (e.Value)
+            var radioButton = sender as RadioButton;
+            if (e.Value == true)
             {
-                _prefs.PrepTimeRange = GetPrepTimeRangeFromRadioButton(sender as RadioButton);
+                // Set the selected cuisine type to preferences
+                string selectedCuisineType = radioButton.Content.ToString();
+
+                //_prefs.CuisineType = selectedCuisineType;
+                _cuisineType = selectedCuisineType;
+                DisplayAlert("Cuisine Type Selected", $"You have selected: {_cuisineType} ", "OK");
             }
+
+
         }
+        // Prep time
+        private async void OnPrepTimeRangeChanged(object sender, CheckedChangedEventArgs e)
+        {
+            var radioButton = sender as RadioButton;
+            if (e.Value == true)
+            {
+                // Set the selected cuisine type to preferences
+                string selectedCuisineType = radioButton.Content.ToString();
+
+                //_prefs.CuisineType = selectedCuisineType;
+                _cuisineType = selectedCuisineType;
+                DisplayAlert("Cuisine Type Selected", $"You have selected: {_cuisineType} ", "OK");
+            }
+
+
+        }
+
 
         // Other Restrictions (example: No-Nuts)
         private void OnNoNutsCheckedChanged(object sender, CheckedChangedEventArgs e)
@@ -61,50 +135,51 @@ namespace FlavorFiesta.Pages
             }
         }
 
-        // Helper methods to get ranges from radio buttons
-        private int GetSugarRangeFromRadioButton(RadioButton radioButton)
-        {
-            switch (radioButton.Content.ToString())
-            {
-                case "0-5":
-                    return 0;
-                case "5-20":
-                    return 1;
-                case "20+":
-                    return 2;
-                default:
-                    return -1; // Handle error case
-            }
-        }
+        //    // Helper methods to get ranges from radio buttons
+        //    private int GetSugarRangeFromRadioButton(RadioButton radioButton)
+        //    {
+        //        switch (radioButton.Content.ToString())
+        //        {
+        //            case "0-5":
+        //                return 0;
+        //            case "5-20":
+        //                return 1;
+        //            case "20+":
+        //                return 2;
+        //            default:
+        //                return -1; // Handle error case
+        //        }
+        //    }
 
-        private int GetServingsRangeFromRadioButton(RadioButton radioButton)
-        {
-            switch (radioButton.Content.ToString())
-            {
-                case "0-2":
-                    return 0;
-                case "2-10":
-                    return 1;
-                case "12+":
-                    return 2;
-                default:
-                    return -1; // Handle error case
-            }
-        }
+        //    private int GetServingsRangeFromRadioButton(RadioButton radioButton)
+        //    {
+        //        switch (radioButton.Content.ToString())
+        //        {
+        //            case "0-2":
+        //                return 0;
+        //            case "2-10":
+        //                return 1;
+        //            case "12+":
+        //                return 2;
+        //            default:
+        //                return -1; // Handle error case
+        //        }
+        //    }
 
-        private TimeSpan GetPrepTimeRangeFromRadioButton(RadioButton radioButton)
-        {
-            switch (radioButton.Content.ToString())
-            {
-                case "15-30":
-                    return TimeSpan.FromMinutes(15);
-                case "30-60":
-                    return TimeSpan.FromMinutes(30);
-                case "60+":
-                    return TimeSpan.FromMinutes(60);
-                default:
-                    return TimeSpan.Zero; // Handle error case
-            }
-        }
+        //    private TimeSpan GetPrepTimeRangeFromRadioButton(RadioButton radioButton)
+        //    {
+        //        switch (radioButton.Content.ToString())
+        //        {
+        //            case "15-30":
+        //                return TimeSpan.FromMinutes(15);
+        //            case "30-60":
+        //                return TimeSpan.FromMinutes(30);
+        //            case "60+":
+        //                return TimeSpan.FromMinutes(60);
+        //            default:
+        //                return TimeSpan.Zero; // Handle error case
+        //        }
+        //    }
+        //}
     }
 }
