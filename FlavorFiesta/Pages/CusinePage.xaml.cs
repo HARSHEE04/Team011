59f9a503c2b3594637a05ccb3096c0e4d88cb9b8
SarasGunjan <saraswag@sheridancollege.ca>
2024-04-12 12:26:12 -0400

    tried debugging cuisine page xaml.cs

--- FlavorFiesta/Pages/CusinePage.xaml.cs
+++ FlavorFiesta/Pages/CusinePage.xaml.cs
@@ -5,29 +5,43 @@ namespace FlavorFiesta.Pages
 {
     public partial class CusinePage : ContentPage
     {
-        private BusinessLogic.Preferences _prefs; // Declare _prefs as an instance variable of type Preferences
+        private string _cuisineType;
+        private string _dietType;
+        //private BusinessLogic.Preferences _prefs; // Declare _prefs as an instance variable of type Preferences
 
         public CusinePage(string DietType) // Accept Preferences as an instance parameter
         {
             InitializeComponent();
+            _dietType = DietType;
+            
             //_prefs = prefs; // Assign the parameter to the instance variable
         }
 
-        private void OnCuisineTypeChanged(object sender, CheckedChangedEventArgs e)
+
+     
+        private async void OnCuisineTypeChanged(object sender, CheckedChangedEventArgs e)
         {
             var radioButton = sender as RadioButton;
-            if (e.Value)
+            if (e.Value == true)
             {
                 // Set the selected cuisine type to preferences
                 string selectedCuisineType = radioButton.Content.ToString();
-                _prefs.CuisineType = selectedCuisineType;
+
+                //_prefs.CuisineType = selectedCuisineType;
+                _cuisineType = selectedCuisineType;
+                DisplayAlert("Cuisine Type Selected", $"You have selected: {_cuisineType} ", "OK");
             }
+            else
+            {
+                DisplayAlert("Cuisine Type Selected", $"You have selected:  ", "OK");
+            }
+
         }
 
         private async void OnSubmitClicked(object sender, EventArgs e)
         {
             // Navigate to the next page with the preferences
-            await Navigation.PushAsync(new RecipeType(_prefs));
+            await Navigation.PushAsync(new RecipeType(_dietType, _cuisineType));
         }
     }
 }
