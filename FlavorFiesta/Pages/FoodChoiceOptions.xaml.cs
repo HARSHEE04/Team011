d5be559f1c35f1f1a76200b6449a76b337f6ad0a
MaryamElhamidi <unicolmaryame@gmail.com>
2024-04-12 00:22:23 -0400

    Removed Errors

--- FlavorFiesta/Pages/FoodChoiceOptions.xaml.cs
+++ FlavorFiesta/Pages/FoodChoiceOptions.xaml.cs
@@ -1,33 +1,46 @@
-﻿using FlavorFiesta.BusinessLogic;
-using FlavorFiesta.DataPersistance;
-namespace FlavorFiesta.Pages;
+﻿using System;
+using System.Collections.Generic;
+using FlavorFiesta.BusinessLogic;
 
-public partial class FoodChoiceOptions : ContentPage
+namespace FlavorFiesta.Pages
 {
-    private FlavorFiesta.BusinessLogic.Preferences _prefs;
-
-
-    public FoodChoiceOptions(FlavorFiesta.BusinessLogic.Preferences prefs)
+    public partial class FoodChoiceOptions : ContentPage
     {
-        InitializeComponent();
-        _prefs = prefs ?? new Preferences();
-    }
+        private FlavorFiesta.BusinessLogic.Preferences _prefs; // Fully qualified the Preferences class with its namespace
 
-    private void OnFoodChoiceChanged(object sender, CheckedChangedEventArgs e)
-    {
-        var radioButton = sender as RadioButton;
-        if (e.Value)
+        public FoodChoiceOptions(FlavorFiesta.BusinessLogic.Preferences prefs)
         {
-            string selectedFoodChoice = radioButton.Content.ToString();
-            DisplayAlert("Food Choice Selected", $"You have selected: {selectedFoodChoice}", "OK");
-
-            // Set the selected diet type to preferences
-            _prefs.DietType = selectedFoodChoice;
+            InitializeComponent();
+            _prefs = prefs ?? new FlavorFiesta.BusinessLogic.Preferences(
+                dietType: "",    // Provide a default value for dietType
+                cusineType: "",  // Provide a default value for cusineType
+                mealType: "",    // Provide a default value for mealType
+                caloriesRange: 0, // Provide a default value for caloriesRange
+                protenRange: 0,  // Provide a default value for protenRange
+                sugarRange: 0,   // Provide a default value for sugarRange
+                serveingsRange: 0, // Provide a default value for serveingsRange
+                prepTimeRange: TimeSpan.Zero, // Provide a default value for prepTimeRange
+                dietaryRestrictions: new List<string>() // Provide an empty list for dietaryRestrictions
+            );
         }
-    }
-    private async void OnSubmitClicked(object sender, EventArgs e)
-    {
-        // Navigate to the next page with the preferences
-        await Navigation.PushAsync(new RecipeType(_prefs));
+
+        private void OnFoodChoiceChanged(object sender, CheckedChangedEventArgs e)
+        {
+            var radioButton = sender as RadioButton;
+            if (e.Value)
+            {
+                string selectedFoodChoice = radioButton.Content.ToString();
+                DisplayAlert("Food Choice Selected", $"You have selected: {selectedFoodChoice}", "OK");
+
+                // Set the selected diet type to preferences
+                _prefs.DietType = selectedFoodChoice;
+            }
+        }
+
+        private async void OnSubmitClicked(object sender, EventArgs e)
+        {
+            // Navigate to the next page with the preferences
+            await Navigation.PushAsync(new RecipeType(_prefs));
+        }
     }
 }
