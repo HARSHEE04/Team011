de8160999b8c2c725a135a7ca7751b6c0e3496c2
MaryamElhamidi <unicolmaryame@gmail.com>
2024-04-12 16:47:26 -0400

    Tried getting choose recipe to work

--- FlavorFiesta/Pages/ChooseRecipe.xaml.cs
+++ FlavorFiesta/Pages/ChooseRecipe.xaml.cs
@@ -12,37 +12,58 @@ public partial class ChooseRecipe : ContentPage
     public ChooseRecipe(BusinessLogic.Preferences prefs)
     {
         InitializeComponent();
-        BindingContext = this;
+        Debug.WriteLine($"Using preferences to search recipes: DietType={prefs.DietType}");
 
         _recipeManager = new RecipeManager();
         _csvRecipes = new RecipeManagerDataPersistance();
 
-        allMatchingRecipes = _recipeManager.SearchRecipe(prefs, _csvRecipes);
-        if (allMatchingRecipes.Count >= 2)
-        {
-            Recipe1Name.Text = allMatchingRecipes[0].Name;
-            Recipe1Image.Source = allMatchingRecipes[0].RecipeImage;
-            Recipe1PrepTme.Text = allMatchingRecipes[0].RecipePreferences.PrepTimeRange.ToString();
+        Debug.WriteLine($"Received preferences: DietType={prefs.DietType}, CuisineType={prefs.CuisineType}, RecipeType={prefs.MealType}");
 
-            Recipe2Name.Text = allMatchingRecipes[1].Name;
-            Recipe2Image.Source = allMatchingRecipes[1].RecipeImage;
-            Recipe2PrepTme.Text = allMatchingRecipes[1].RecipePreferences.PrepTimeRange.ToString();
-        }
-        else
+        try
         {
-            Debug.WriteLine("Not enough recipes found");
+            allMatchingRecipes = _recipeManager.SearchRecipe(prefs, _csvRecipes);
+            Debug.WriteLine($"Found {allMatchingRecipes.Count} matching recipes.");
+
+            if (allMatchingRecipes.Count >= 2)
+            {
+                // Assuming there are at least two recipes to display
+                Recipe1Name.Text = allMatchingRecipes[0].Name;
+                Recipe1Image.Source = allMatchingRecipes[0].RecipeImage;
+                Recipe1PrepTme.Text = allMatchingRecipes[0].RecipePreferences.PrepTimeRange.ToString();
+
+                Recipe2Name.Text = allMatchingRecipes[1].Name;
+                Recipe2Image.Source = allMatchingRecipes[1].RecipeImage;
+                Recipe2PrepTme.Text = allMatchingRecipes[1].RecipePreferences.PrepTimeRange.ToString();
+            }
+            else
+            {
+                // Handle the scenario where fewer than two recipes are found
+                Debug.WriteLine("Not enough recipes found to display.");
+                DisplayAlert("Notice", "Not enough recipes found based on your preferences.", "OK");
+            }
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine($"Error searching recipes: {ex.Message}");
+            DisplayAlert("Error", "Failed to retrieve recipes.", "OK");
         }
     }
 
-    private void OnRecipe1Chosen(object sender, EventArgs e)
+    private async void OnRecipe1Chosen(object sender, EventArgs e)
     {
-        Recipe selectedRecipe = allMatchingRecipes[0];
-        Navigation.PushAsync(new DisplayRecipe(selectedRecipe));
+        if (allMatchingRecipes.Count > 0)
+        {
+            Recipe selectedRecipe = allMatchingRecipes[0];
+            await Navigation.PushAsync(new DisplayRecipe(selectedRecipe));
+        }
     }
 
-    private void OnRecipe2Chosen(object sender, EventArgs e)
+    private async void OnRecipe2Chosen(object sender, EventArgs e)
     {
-        Recipe selectedRecipe = allMatchingRecipes[1];
-        Navigation.PushAsync(new DisplayRecipe(selectedRecipe));
+        if (allMatchingRecipes.Count > 1)
+        {
+            Recipe selectedRecipe = allMatchingRecipes[1];
+            await Navigation.PushAsync(new DisplayRecipe(selectedRecipe));
+        }
     }
 }
