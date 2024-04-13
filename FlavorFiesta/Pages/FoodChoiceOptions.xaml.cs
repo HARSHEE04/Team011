926932ab8484562feceea29c388cce6324991622
MaryamElhamidi <unicolmaryame@gmail.com>
2024-04-12 20:05:13 -0400

    removed comment

--- FlavorFiesta/Pages/FoodChoiceOptions.xaml.cs
+++ FlavorFiesta/Pages/FoodChoiceOptions.xaml.cs
@@ -9,33 +9,15 @@ namespace FlavorFiesta.Pages
     public partial class FoodChoiceOptions : ContentPage
     {
         private string _dietType;
-        // private BusinessLogic.Preferences _preferences;
-        // private BusinessLogic.PreferencesManager _preferencesManager;
-
 
         public FoodChoiceOptions()
         {
             InitializeComponent();
-            //_prefManager = new PrefManagerDataPersistence(Path.Combine(FileSystem.AppDataDirectory, "preferences.json"));
-
         }
 
         public FoodChoiceOptions(string dietType, string cusineType, string mealType, int caloriesRange, int proteinRange, int sugarRange, int servingsRange, TimeSpan prepTimeRange, List<string> dietaryRestrictions)
         {
             InitializeComponent();
-            //_prefs = new FlavorFiesta.BusinessLogic.Preferences(
-            //    dietType: dietType,
-            //    cusineType: cusineType,
-            //    mealType: mealType,
-            //    caloriesRange: caloriesRange,
-            //    protenRange: proteinRange,
-            //    sugarRange: sugarRange,
-            //    serveingsRange: servingsRange,
-            //    prepTimeRange: prepTimeRange,
-            //    dietaryRestrictions: dietaryRestrictions
-            //);
-            //_prefManager = new PrefManagerDataPersistence(Path.Combine(FileSystem.AppDataDirectory, "preferences.json"));
-
         }
 
         private async void OnFoodChoiceChanged(object sender, CheckedChangedEventArgs e)
@@ -44,7 +26,7 @@ namespace FlavorFiesta.Pages
             if (e.Value)
             {
                 _dietType = radioButton.Content.ToString();
-                DisplayAlert("Food choice Option", $"You have selected {_dietType}", "Ok");
+                await DisplayAlert("Food choice Option", $"You have selected {_dietType}", "Ok");
             }
             await Task.CompletedTask;
         }
