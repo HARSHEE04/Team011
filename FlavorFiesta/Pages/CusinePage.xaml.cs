60d115aed75c65a1de983fe248e611f11547d6ed
MaryamElhamidi <unicolmaryame@gmail.com>
2024-04-12 22:12:14 -0400

    Added xml comment to cusinepage

--- FlavorFiesta/Pages/CusinePage.xaml.cs
+++ FlavorFiesta/Pages/CusinePage.xaml.cs
@@ -3,17 +3,27 @@ using System;
 
 namespace FlavorFiesta.Pages
 {
+    /// <summary>
+    /// Represents a page for selecting cuisine preferences.
+    /// </summary>
     public partial class CusinePage : ContentPage
     {
         private string _cuisineType;
         private string _dietType;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CusinePage"/> class.
+        /// </summary>
+        /// <param name="DietType">The type of diet from user preferences.</param>
         public CusinePage(string DietType) // Accept Preferences as an instance parameter
         {
             InitializeComponent();
             _dietType = DietType;
         }
 
+        /// <summary>
+        /// Event handler for when a cuisine type is changed.
+        /// </summary>
         private async void OnCuisineTypeChanged(object sender, CheckedChangedEventArgs e)
         {
             var radioButton = sender as RadioButton;
@@ -26,6 +36,9 @@ namespace FlavorFiesta.Pages
             }
         }
 
+        /// <summary>
+        /// Event handler for when the submit button is clicked.
+        /// </summary>
         private async void OnSubmitClicked(object sender, EventArgs e)
         {
             if (!string.IsNullOrEmpty(_cuisineType))
